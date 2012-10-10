d0859d89bf59ab3546de638f7cf3590850b22d14
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2012-10-10 12:50:57 +0200

    Restrict the library to PoliMorf

--- Data/PoliMorf.hs
+++ Data/PoliMorf.hs
@@ -1,97 +1,149 @@
 {-# LANGUAGE PatternGuards #-}
 {-# LANGUAGE BangPatterns #-}
 
+-- | The module provides functionality for manipulating PoliMorf, the
+-- morphological dictionary for Polish. Apart from IO utilities there
+-- is a 'merge' function which can be used to merge the PoliMorf with
+-- another dictionary resources.
+
 module Data.PoliMorf
-( PoliMorf
-, joinDict
+( 
+-- * Types
+  Form
+, Base
+, Tag
+, Entry (..)
+
+-- * Parsing
+, readPoliMorf
+, parsePoliMorf
+
+-- * Merging
+, BaseMap
+, mkBaseMap
 , RelCode (..)
+, merge
 ) where
 
-import qualified Data.Map as M
-import qualified Data.Set as S
-import qualified Data.Text as T
+import Control.Applicative ((<$>), (<*>))
 import Data.Monoid (Monoid, mappend)
 import Data.List (foldl')
 import Data.Maybe (maybeToList)
-
 import Data.Binary (Binary, get, put)
+import qualified Data.Map as M
+import qualified Data.Set as S
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as L
+import qualified Data.Text.Lazy.IO as L
 
-type Form  = T.Text
-type Lemma = T.Text
-type PoliMorf = M.Map Form [Lemma]
+-- | A form.
+type Form = T.Text
+
+-- | A base form.
+type Base = T.Text
+
+-- | A morphosyntactic tag.
+type Tag  = T.Text
+
+-- | An entry from the PoliMorf dictionary.
+data Entry = Entry
+    { form :: !Form
+    , base :: !Base
+    , tag  :: !Tag }
+
+-- | Read the PoliMorf from the file.
+readPoliMorf :: FilePath -> IO [Entry]
+readPoliMorf path = parsePoliMorf <$> L.readFile path
+
+-- | Parse the PoliMorf into a list of entries.
+parsePoliMorf :: L.Text -> [Entry]
+parsePoliMorf = map parsePoliRow . L.lines 
+
+-- | Get an entry pair from a PoliMorf row.
+parsePoliRow :: L.Text -> Entry
+parsePoliRow row = case map L.toStrict (L.split (=='\t') row) of
+    [_form, _base, _tag] -> Entry _form _base _tag
+    _   -> error $ "parsePoliRow: invalid row \"" ++ L.unpack row ++ "\""
+
+-- | A map from forms to their possible base forms (there may be many since
+-- the form may be a member of multiple lexemes).
+type BaseMap = M.Map Form [Base]
+
+-- | Make the base map from the list of entries.
+mkBaseMap :: [Entry] -> BaseMap
+mkBaseMap = M.fromListWith (++) . map ((,) <$> form <*> (:[]) . base)
 
 -- | Reliability information: how did we assign a particular label to
 -- a particular word form.
 data RelCode
-    = Exact
-    | ByLemma   -- ^ Label assigned based on lemma label  
-    | ByForm    -- ^ Based on labels of other forms within the same lexeme.
+    = Exact     -- ^ Label assigned in a direct manner
+    | ByBase    -- ^ Label assigned based on a lemma label  
+    | ByForm    -- ^ Based on labels of other forms within the same lexeme
     deriving (Eq, Ord, Show, Read)
 
 instance Binary RelCode where
     put Exact   = put '1'
-    put ByLemma = put '2'
+    put ByBase = put '2'
     put ByForm  = put '3'
     get = get >>= \x -> return $ case x of
         '1' -> Exact
-        '2' -> ByLemma
+        '2' -> ByBase
         '3' -> ByForm
+        c   -> error $ "get: invalid RelCode code '" ++ [c] ++ "'"
 
-joinDict :: Monoid m => PoliMorf -> M.Map Form m
-         -> M.Map Form (Maybe (m, RelCode))
-joinDict poli dict0 =
-    combine poli dict0 f
+-- | Merge the 'BaseMap' with the dictionary resource which maps forms to
+-- monoidal labels.  Depending on the inference technique there are three
+-- kinds of labels in the resultant dictionary:
+-- 'Exact' labels assigned in a direct manner, 'ByBase' labels assigned
+-- to all forms which have a base form with a label in the input dictionary,
+-- and 'ByForm' labels assigned to all forms which have a related form from the
+-- same lexeme with a label in the input dictionary.
+--
+-- For a particular form in the output dictionary there are labels extracted
+-- with at most one of the methods described above, with 'Exact' labels
+-- having a precedence over 'ByBase' labels and 'ByBase' labels having
+-- a precedence over 'ByForm' labels.
+--
+-- This function is far from being memory efficient right now.  If you plan to
+-- run it with respect to the entire PoliMorf dictionary you should do it
+-- on a machine with an abundance of available memory.
+merge :: Monoid m => BaseMap -> M.Map Form m -> M.Map Form (Maybe (m, RelCode))
+merge poli dict0 =
+    M.fromList [(x, combine x) | x <- keys]
   where
-    f x | Just y <- M.lookup x dict0 = Just (y, Exact)
-        | Just y <- M.lookup x dict1 = Just (y, ByLemma)
+    -- Keys in the output dictionary.
+    keys = S.toList (M.keysSet poli `S.union` M.keysSet dict0)
+
+    -- Combining function.
+    combine x
+        | Just y <- M.lookup x dict0 = Just (y, Exact)
+        | Just y <- M.lookup x dict1 = Just (y, ByBase)
         | Just y <- M.lookup x dict2 = Just (y, ByForm)
         | otherwise = Nothing
 
-    -- | Extended to all base forms of dict0 keys.
+    -- Extended to all base forms of dict0 keys.
     dict1 = fromListWith mappend
         [ (lemma, x)
-        | (form, x) <- M.assocs dict0
-        , lemmas <- maybeToList (form `M.lookup` poli)
+        | (_form, x) <- M.assocs dict0
+        , lemmas <- maybeToList (_form `M.lookup` poli)
         , lemma <- lemmas ]
 
-    -- | Extended to all forms of dict0 keys
+    -- Extended to all forms of dict0 keys.
     dict2 = fromListWith mappend
         [ (form', x)
-        | (form, x) <- M.assocs dict0
-        , lemmas <- maybeToList (form `M.lookup` poli)
+        | (_form, x) <- M.assocs dict0
+        , lemmas <- maybeToList (_form `M.lookup` poli)
         , lemma  <- lemmas
         , forms' <- maybeToList (lemma `M.lookup` ilop)
         , form'  <- forms' ]
 
-    -- | Inverse poli dictionary.
+    -- Inverse poli dictionary.
     ilop = fmap S.toList $ fromListWith mappend
-        [ (lemma, S.singleton form)
-        | (form, lemmas) <- M.assocs poli
+        [ (lemma, S.singleton _form)
+        | (_form, lemmas) <- M.assocs poli
         , lemma <- lemmas ]
-
-combine :: Ord a => M.Map a b -> M.Map a c -> (a -> d) -> M.Map a d
-combine m n f =
-    let keys = S.toList (M.keysSet m `S.union` M.keysSet n)
-    in  M.fromList [(x, f x) | x <- keys]
 
 fromListWith :: Ord k => (a -> a -> a) -> [(k, a)] -> M.Map k a
 fromListWith f xs =
     let update m (!k, !x) = M.insertWith' f k x m
     in  foldl' update M.empty xs
-
--- joinHist :: Ord a => PoliMorf -> M.Map Form [a]
---          -> M.Map Form (Maybe ([a], RelCode))
--- joinHist poli hist =
---     combine poli hist f
---   where
---     f x | Just y <- M.lookup x hist
---             = Just (y, Exact)
---         | Just y <- M.lookup x poli >>= flip M.lookup hist
---             = Just (y, ByLemma)
---         | Just y <- M.lookup x poli >>= flip M.lookup hist'
---             = Just (y, ByForm)
---         | otherwise = Nothing
---     hist' = M.fromList
---         [ (lemma, y)
---         | (form, lemma) <- M.assocs poli
---         , y <- maybeToList (form `M.lookup` hist) ]
