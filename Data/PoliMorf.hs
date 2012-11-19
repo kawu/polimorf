8363af1a0779dfeec538cf3e34291fc69573e703
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2012-11-19 19:51:29 +0100

    Add possibility to create form generation map

--- Data/PoliMorf.hs
+++ Data/PoliMorf.hs
@@ -9,12 +9,13 @@
 
 module Data.PoliMorf
 ( 
--- * Types
+-- * Core types
   Form
 , Base
 , Tag
 , Cat
 , Entry (..)
+, atomic
 
 -- * Parsing
 , readPoliMorf
@@ -24,13 +25,15 @@ module Data.PoliMorf
 , Rule (..)
 , apply
 , toBase
-, BaseMap
+-- , BaseMap
+, mkRuleMap
 , mkBaseMap
+, mkGenMap
 -- , RelCode (..)
 -- , merge
 ) where
 
-import Control.Applicative (pure, (<$>), (<*>))
+import Control.Applicative ((<$>), (<*>))
 import Data.Maybe (catMaybes)
 import Data.Monoid (mappend)
 import Data.List (foldl')
@@ -62,6 +65,13 @@ data Entry = Entry
     , tag  :: !Tag
     , cat  :: !Cat }
     deriving (Eq, Ord, Show, Read)
+
+-- | Is the entry an atomic one?
+atomic :: Entry -> Bool
+atomic x
+    | "sup" `T.isInfixOf` tag x && "naj" `T.isPrefixOf` form x = False
+    | "neg" `T.isInfixOf` tag x && "nie" `T.isPrefixOf` form x = False
+    | otherwise = True
 
 -- | Read the PoliMorf from the file.
 readPoliMorf :: FilePath -> IO [Entry]
@@ -102,60 +112,67 @@ toBase x
         Just (c, _, _)  -> T.length c
         Nothing         -> trace (show (form x, base x)) 0
 
--- betweenWith :: T.Text -> T.Text -> T.Text -> Maybe Rule
--- betweenWith specPref x y = if specPref `T.isPrefixOf` x
---     then nonZero (between (T.drop (T.length specPref) x) y)
---     else Nothing
--- 
--- nonZero :: Int -> Rule -> Maybe Rule
--- nonZero r
---     | cut r -- 
+-- | Make a rule to trasnalte between two strings.
+between :: T.Text -> T.Text -> Rule
+between source dest =
+    let k = lcp source dest
+    in  Rule (T.length source - k) (T.drop k dest)
+  where
+    lcp a b = case T.commonPrefixes a b of
+        Just (c, _, _)  -> T.length c
+        Nothing         -> 0
 
--- | A map from forms to their possible base forms (there may be many since
--- the form may be a member of multiple lexemes).
-type BaseMap = D.DAWG (S.Set Rule)
+-- -- | A map from forms to their possible base forms (there may be many since
+-- -- the form may be a member of multiple lexemes).
+-- type BaseMap = D.DAWG (S.Set Rule)
 
--- | Make the base map from the list of entries.
-mkBaseMap :: [Entry] -> BaseMap
-mkBaseMap xs = D.fromListWith S.union $ catMaybes
-    [ (,) <$> pure (T.unpack (form x))
-          <*> (S.singleton <$> toBase x)
-    | x <- xs ]
+-- | Make the rule map from a list of entries.
+mkRuleMap :: [(T.Text, T.Text)] -> D.DAWG (S.Set Rule)
+mkRuleMap xs = D.fromListWith S.union $
+    [ ( T.unpack x
+      , S.singleton (between x y) )
+    | (x, y) <- xs ]
 
--- -- | Reliability information: how did we assign a particular label to
--- -- a particular word form.
--- data RelCode
---     = Exact     -- ^ Label assigned in a direct manner
---     | ByBase    -- ^ Label assigned based on a lemma label  
---     | ByForm    -- ^ Based on labels of other forms within the same lexeme
---     deriving (Eq, Ord, Show, Read)
--- 
--- instance Binary RelCode where
---     put Exact   = put '1'
---     put ByBase = put '2'
---     put ByForm  = put '3'
---     get = get >>= \x -> return $ case x of
---         '1' -> Exact
---         '2' -> ByBase
---         '3' -> ByForm
---         c   -> error $ "get: invalid RelCode code '" ++ [c] ++ "'"
--- 
--- -- | Merge the 'BaseMap' with the dictionary resource which maps forms to
--- -- sets of labels.  Every label is assigned a 'RelCode' which tells what
--- -- is the relation between the label and the form.  There are three
--- -- kinds of labels:
--- -- 'Exact' labels assigned in a direct manner, 'ByBase' labels assigned
--- -- to all forms which have a base form with a label in the input dictionary,
--- -- and 'ByForm' labels assigned to all forms which have a related form from the
--- -- same lexeme with a label in the input dictionary.
--- --
--- -- This function is far from being memory efficient right now.  If you plan to
--- -- run it with respect to the entire PoliMorf dictionary you should do it
--- -- on a machine with an abundance of available memory.
+mkBaseMap :: [Entry] -> D.DAWG (S.Set Rule)
+mkBaseMap = mkRuleMap . map ((,) <$> form <*> base)
+
+mkGenMap :: [Entry] -> D.DAWG (S.Set Rule)
+mkGenMap = mkRuleMap . map ((,) <$> base <*> form)
+
+-- | Reliability information: how did we assign a particular label to
+-- a particular word form.
+data RelCode
+    = Exact     -- ^ Label assigned in a direct manner
+    | ByBase    -- ^ Label assigned based on a lemma label  
+    | ByForm    -- ^ Based on labels of other forms within the same lexeme
+    deriving (Eq, Ord, Show, Read)
+
+instance Binary RelCode where
+    put Exact   = put '1'
+    put ByBase = put '2'
+    put ByForm  = put '3'
+    get = get >>= \x -> return $ case x of
+        '1' -> Exact
+        '2' -> ByBase
+        '3' -> ByForm
+        c   -> error $ "get: invalid RelCode code '" ++ [c] ++ "'"
+
+-- | Merge the 'BaseMap' with the dictionary resource which maps forms to
+-- sets of labels.  Every label is assigned a 'RelCode' which tells what
+-- is the relation between the label and the form.  There are three
+-- kinds of labels:
+-- 'Exact' labels assigned in a direct manner, 'ByBase' labels assigned
+-- to all forms which have a base form with a label in the input dictionary,
+-- and 'ByForm' labels assigned to all forms which have a related form from the
+-- same lexeme with a label in the input dictionary.
+--
+-- This function is far from being memory efficient right now.  If you plan to
+-- run it with respect to the entire PoliMorf dictionary you should do it
+-- on a machine with an abundance of available memory.
 -- merge
 --     :: Ord a => BaseMap
---     -> M.Map Form (S.Set a)
---     -> M.Map Form (M.Map a RelCode)
+--     -> D.DAWG (S.Set a)
+--     -> D.DAWG (M.Map a RelCode)
 -- merge poli dict0 = M.fromList
 --     [ (x, combine x)
 --     | x <- keys ]
@@ -191,8 +208,8 @@ mkBaseMap xs = D.fromListWith S.union $ catMaybes
 --         | (_form, lemmas) <- M.assocs poli
 --         , lemma <- S.toList lemmas ]
 -- 
--- elemsOn :: (Ord a, Ord b) => M.Map a (S.Set b) -> a -> [b]
--- elemsOn m x = case x `M.lookup` m of
+-- elemsOn :: Ord b => D.DAWG (S.Set b) -> String -> [b]
+-- elemsOn m x = case x `D.lookup` m of
 --     Just s  -> S.toList s
 --     Nothing -> []
 -- 
